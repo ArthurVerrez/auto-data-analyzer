543fd2ec8c0760ccaf1e05174d7f6dcbc4d5fc90
Arthur Verrez <arthurverrez@gmail.com>
2025-03-02 23:40:41 +0100

    feat: add more statistics

--- sections/response.py
+++ sections/response.py
@@ -25,58 +25,74 @@ def response() -> None:
             sample_value = None
             value_type = "NoneType"
 
-        st.write(f"**{col}**: ({value_type}), eg: `{sample_value}`")
-
         distinct = df[col].nunique(dropna=True)
         mode = df[col].mode().iloc[0] if not df[col].mode().empty else None
+        num_missing = df[col].isnull().sum()
 
         # Calculate statistics based on column type.
         if pd.api.types.is_numeric_dtype(df[col]):
             stats = {
+                "Name": col,
+                "Type": value_type,
+                "Example": sample_value,
                 "Min": df[col].min(),
                 "Max": df[col].max(),
                 "Mean": df[col].mean(),
                 "Median": df[col].median(),
                 "Std": df[col].std(),
                 "Distinct Values": distinct,
+                "Sum": df[col].sum(),
                 "Mode": mode,
+                "Missing Values": num_missing,
             }
-            st.write("**Statistics:**")
-            st.write(stats)
         elif pd.api.types.is_datetime64_any_dtype(df[col]):
 
             stats = {
+                "Name": col,
+                "Type": value_type,
+                "Example": sample_value,
                 "Min": df[col].min(),
                 "Max": df[col].max(),
                 "Distinct Values": distinct,
                 "Mode": mode,
+                "Missing Values": num_missing,
             }
-            st.write("**Statistics:**")
-            st.write(stats)
         elif pd.api.types.is_bool_dtype(df[col]):
             true_count = df[col].sum()
             false_count = (~df[col]).sum() if df[col].dtype == bool else None
-            stats = {"True count": true_count, "False count": false_count}
-            st.write("**Statistics:**")
-            st.write(stats)
+            stats = {
+                "Name": col,
+                "Type": value_type,
+                "Example": sample_value,
+                "True count": true_count,
+                "False count": false_count,
+                "Missing Values": num_missing,
+            }
         # If string, add the average, min, max, median length of the strings
         elif pd.api.types.is_string_dtype(df[col]):
             stats = {
+                "Name": col,
+                "Type": value_type,
+                "Example": sample_value,
                 "Min Length": df[col].str.len().min(),
                 "Max Length": df[col].str.len().max(),
                 "Mean Length": df[col].str.len().mean(),
                 "Median Length": df[col].str.len().median(),
                 "Distinct Values": distinct,
                 "Mode": mode,
+                "Missing Values": num_missing,
             }
-            st.write("**Statistics:**")
-            st.write(stats)
         else:
 
-            stats = {"Distinct Values": distinct, "Mode": mode}
-            st.write("**Statistics:**")
-            st.write(stats)
-        st.write("---")
+            stats = {
+                "Name": col,
+                "Type": value_type,
+                "Example": sample_value,
+                "Distinct Values": distinct,
+                "Mode": mode,
+                "Missing Values": num_missing,
+            }
+        st.write(stats)
 
     st.subheader("Random Full Rows")
     if not df.empty:
